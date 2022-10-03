1ccfb0a768cb6d91586fe30d32bceedf5026fc81
prematuronly99999 <113847017+prematuronly99999@users.noreply.github.com>
2022-10-03 13:36:49 +0700

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -15,7 +15,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://youtube.com/c/HwModsWa857'
 global.ig = 'https://www.instagram.com/hikal_857/?hl=id'
-global.mygc = 'wa.me/6289503267904'
+global.mygc = 'wa.me/6285714170944'
 global.myweb = 'https://eclass.iainsalatiga.ac.id/app/upload/users/1/10892/my_files/XhiroMhonshine.html'
 global.linkgrupss = "https://chat.whatsapp.com/Jh7zaCSnPCBC4gdYOEVm0K"
 global.linkpro = 'mailto:support@whatsapp.com?subject=Deactivate%20My%20Account%20Number&body=Please%20Deactivate%20The%20My%20Account%20Number%20%2B62XXXXXXXX%20Immediately%20Because%20The%20Number%20Has%20Been%20Lost'
@@ -27,11 +27,11 @@ global.botname = 'HW MODS WA' //ubah jadi nama bot mu, note tanda ' gausah di ha
 global.footer = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6289503267904']
-global.premium = ['6289503267904']
-global.packname = '『  Vyn 』   MazTar'
-global.ownerr = ['『  Vyn 』   MazTar']
-global.author = '『  Vyn 』   MazTar'
+global.owner = ['6281214281312']
+global.premium = ['6281214281312']
+global.packname = 'HW MODS WA'
+global.ownerr = ['HW MODS WA']
+global.author = 'HW MODS WA'
 global.sessionName = './sessionye/haikal'
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sp = '⭔'
